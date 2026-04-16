4b64a3251ce40ead419407b542d47b8d211ed78b
Alica Goncharenko <galisak3@gmail.com>
2026-04-17 00:17:29 +0300

    Done Lab 10

---
+++ lab10/prj/ModulesHoncharenko/ModulesHoncharenko.h
@@ -0,0 +1,11 @@
+#ifndef MODULESHONCHARENKO_H
+#define MODULESHONCHARENKO_H
+
+#include <string>
+
+double s_calculation(double x, double y, double z);
+void Task10_1(const std::string& inFileName, const std::string& outFileName);
+void Task10_2(const std::string& inFileName, const std::string& outFileName);
+void Task10_3(const std::string& outFileName, double x, double y, double z, unsigned int b);
+
+#endif
